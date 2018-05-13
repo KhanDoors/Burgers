9f9c23dd24ae1269125beb34bd3a6d6e561060b7
unknown <syyadkhan@yahoo.com>
2018-05-13 13:49:40 -0400

    may131349

--- config/connection.js
+++ config/connection.js
@@ -8,9 +8,9 @@ if (process.env.JAWSDB_URL) {
 } else {
 	connection = mysql.createConnection({
         host: 'localhost',
-        port: 3000,
+        port: 3306,
 		user: 'root',
-		password: '',
+		password: 'Cowboys1',
 		database: 'burgers_db'
 	});
 };
